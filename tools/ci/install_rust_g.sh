3fab10b5640c7ba538887f3446019f9606a1240b
san7890 <the@san7890.com>
2023-07-09 01:02:36 -0600

    Revert "let's try pinning it to 22.04"
    This reverts commit cea57ff47f3cb81e8886547a65ff68b84f61713b.

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin
-# wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so" DEBUGGING DO NOT MERGE DO NOT MERGE
-wget -nv -O ~/.byond/bin/librust_g.so "https://file.house/_LOl.so"
+wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
