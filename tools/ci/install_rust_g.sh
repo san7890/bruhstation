d10ad47edc5548f264070677f785a48f6252348b
san7890 <the@san7890.com>
2023-07-09 01:39:10 -0600

    ii'm conchfusion

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -5,6 +5,19 @@ source dependencies.sh
 
 ldd --version
 
+# Rust-g needs a special GLIBC version (dubious - dispute), which for some reason doesn't ship on GitHub Actions Ubuntu 22.04??? Let's get it in here.
+mkdir ~/tmp/glibc
+cd ~/tmp/glibc
+wget --no-check-certificate "https://ftp.gnu.org/gnu/glibc/glibc-$GLIBC_VERSION.tar.gz"
+tar -xvf glibc-$GLIBC_VERSION.tar.gz
+cd glibc-$GLIBC_VERSION
+mkdir build && cd build
+../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include --with-binutils=/usr/bin
+make&&make install
+
+ldd --version
+
+# Now, this is rust-g specific.
 sudo dpkg --add-architecture i386
 sudo apt-get update || true
 sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386
