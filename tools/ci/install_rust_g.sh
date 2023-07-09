3875280b0f3167b84c8c799452b4d9ff9c0b4345
san7890 <the@san7890.com>
2023-07-09 01:48:25 -0600

    specs for i386????
    i'm going to bed after this

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -5,6 +5,8 @@ source dependencies.sh
 
 ldd --version
 
+sudo dpkg --add-architecture i386
+
 # Rust-g needs a special GLIBC version (dubious - dispute), which for some reason doesn't ship on GitHub Actions Ubuntu 22.04??? Let's get it in here.
 mkdir -p ~/tmp/glibc
 cd ~/tmp/glibc
@@ -12,13 +14,11 @@ wget --no-check-certificate "https://ftp.gnu.org/gnu/glibc/glibc-$GLIBC_VERSION.
 tar -xvf glibc-$GLIBC_VERSION.tar.gz
 cd glibc-$GLIBC_VERSION
 mkdir build && cd build
-../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include --with-binutils=/usr/bin
-make&&make install
+../configure --prefix=/usr --disable-profile --enable-add-ons --with-headers=/usr/include --with-binutils=/usr/bin CC='gcc -U__i386'
+make && make install
 
 ldd --version
 
-# Now, this is rust-g specific.
-sudo dpkg --add-architecture i386
 sudo apt-get update || true
 sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386
 sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386
