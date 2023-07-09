ec2a932a448db9721967f9ca141a1c0701c50018
san7890 <the@san7890.com>
2023-07-09 01:13:13 -0600

    perhaps like this?

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 source dependencies.sh
 
 sudo dpkg --add-architecture i386
-sudo apt update || true
-sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386 libgcc-s1:i386
+sudo apt-get update || true
+sudo apt-get install libgcc-s1:i386 g++-multilib zlib1g-dev:i386 libssl-dev:i386
+sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386
 
 mkdir -p ~/.byond/bin
 wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
